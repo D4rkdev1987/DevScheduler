ee1f44b08b7885c4dfa112ab5202f5d877f38075
Benjamin Nicklaus <Marineepo87@gmail.com>
2019-02-26 15:30:38 -0500

    second commit

---
+++ src/components/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className='Home'>
+        <h1>Course Scheduler</h1>
+        <h2>React Redux</h2>
+      </div>
+    );
+  }
+}
+
+export default Home;
